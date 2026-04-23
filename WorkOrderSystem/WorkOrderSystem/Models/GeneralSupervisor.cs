9fa3737b2f52ddb429ae9a1b5788e7e5aeafd13e
Dr-Ivan02 <20251875@itla.edu.do>
2026-04-23 18:25:29 -0400

    Add GeneralSupervisor role implementation

---
+++ WorkOrderSystem/WorkOrderSystem/Models/GeneralSupervisor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkOrderSystem.Models
+{
+    // Represents the general supervisor who manages all work orders
+    public class GeneralSupervisor : Role
+    {
+        public GeneralSupervisor() : base("General Supervisor")
+        {
+        }
+
+        public override void ShowMenu()
+        {
+            Console.WriteLine("1. Create Work Order");
+            Console.WriteLine("2. View All Work Orders");
+        }
+    }
+}
