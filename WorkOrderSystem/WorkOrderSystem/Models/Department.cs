80d15eb7da910435dc1eb36f40d92d5493b505d4
Dr-Ivan02 <20251875@itla.edu.do>
2026-04-23 17:03:46 -0400

    Add Department model
    Adding the departments class to the models

---
+++ WorkOrderSystem/WorkOrderSystem/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkOrderSystem.Models
+{
+    // Represents a department within the organization
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
