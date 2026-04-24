a7c6c2fcce03c47db6973bf6886b1aef59ef028d
Dr-Ivan02 <20251875@itla.edu.do>
2026-04-24 15:09:21 -0400

    Add comment management to WorkOrderService
    Added the services: AddComments, GetCommentsByWorkOrder

--- WorkOrderSystem/WorkOrderSystem/Services/WorkOrderService.cs
+++ WorkOrderSystem/WorkOrderSystem/Services/WorkOrderService.cs
@@ -9,6 +9,7 @@ namespace WorkOrderSystem.Services
     public class WorkOrderService
     {
         private List<WorkOrder> workOrders = new List<WorkOrder>();
+        private List<Comment> comments = new List<Comment>();
 
         public void CreateWorkOrder(WorkOrder order)
         {
@@ -50,5 +51,30 @@ namespace WorkOrderSystem.Services
                 }
             }
         }
+
+        // Adds a comment to a specific work order for tracking updates
+        public void AddCommentToWorkOrder(int workOrderId, string text)
+        {
+            Comment comment = new Comment(workOrderId, text);
+            comments.Add(comment);
+        }
+
+        // Retrieves all comments associated with a specific work order for better communication and tracking
+        public List<Comment> GetCommentsByWorkOrder(int workOrderId)
+        {
+            List<Comment> result = new List<Comment>();
+
+            foreach (var comment in comments)
+            {
+                if (comment.WorkOrderId == workOrderId)
+                {
+                    result.Add(comment);
+                }
+            }
+
+            return result;
+        }
+
+
     }
 }
