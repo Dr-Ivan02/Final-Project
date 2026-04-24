a763cd5ce566bd2eccadba3782a6c8016ec03b10
Dr-Ivan02 <20251875@itla.edu.do>
2026-04-24 18:51:18 -0400

    Setup EF Core DbContext with SQL Server connection
    Database is already created and conected

---
+++ WorkOrderSystem/WorkOrderSystem/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkOrderSystem.Models;
+
+namespace WorkOrderSystem.Data
+{
+    // Database context for the system
+    public class AppDbContext : DbContext
+    {
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=.\\SQLEXPRESS;Database=WorkOrderDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
