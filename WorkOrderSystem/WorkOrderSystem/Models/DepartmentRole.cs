18586332b3e62bcc578adb9b72c4265b09693c02
Dr-Ivan02 <20251875@itla.edu.do>
2026-04-24 12:13:58 -0400

    Add DepartmentRole implementation
    Created the dept role for the use of the several depts

---
+++ WorkOrderSystem/WorkOrderSystem/Models/DepartmentRole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkOrderSystem.Models
+{
+    // Represents a department-level role that interacts with assigned work orders
+    public class DepartmentRole : Role
+    {
+        public int DepartmentId { get; set; }
+
+        public DepartmentRole(int departmentId, string roleName) : base(roleName)
+        {
+            DepartmentId = departmentId;
+        }
+
+        public override void ShowMenu()
+        {
+            Console.WriteLine("1. View Assigned Work Orders");
+            Console.WriteLine("2. Update Work Order Status");
+            Console.WriteLine("3. Add Comment to Work Order");
+        }
+    }
+}
