c10750e28e1d093cbd600bca3cf33cdcc7db4b3f
Dr-Ivan02 <20251875@itla.edu.do>
2026-04-24 12:40:52 -0400

    Add basic WorkOrderService with in-memory storage
    Started to create the services that are going to be used.

---
+++ WorkOrderSystem/WorkOrderSystem/Services/WorkOrderService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkOrderSystem.Models;
+
+namespace WorkOrderSystem.Services
+{
+    // Handles operations related to work orders
+    public class WorkOrderService
+    {
+        private List<WorkOrder> workOrders = new List<WorkOrder>();
+
+        public void CreateWorkOrder(WorkOrder order)
+        {
+            workOrders.Add(order);
+        }
+
+        public List<WorkOrder> GetAllWorkOrders()
+        {
+            return workOrders;
+        }
+    }
+}
