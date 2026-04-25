5c2aa852f7ba4dc9dfdf981d014c1cf767699c87
Dr-Ivan02 <20251875@itla.edu.do>
2026-04-25 00:35:50 -0400

    Creating program.cs
    Intialized the creation of the program.cs, it is supposed to be continued as it is not complete yet

--- WorkOrderSystem/WorkOrderSystem/Program.cs
+++ WorkOrderSystem/WorkOrderSystem/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("");
+﻿using WorkOrderSystem.Services;
+
+var service = new WorkOrderService();
+
+string option;
+
+do
+{
+    Console.WriteLine("\n--- WORK ORDER SYSTEM ---");
+    Console.WriteLine("1. Create Work Order");
+    Console.WriteLine("2. View All Work Orders");
+    Console.WriteLine("3. Update Work Order Status");
+    Console.WriteLine("4. Add Comment");
+    Console.WriteLine("5. View Comments");
+    Console.WriteLine("0. Exit");
+
+    Console.Write("Select an option: ");
+    option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            // Method to create order
+            break;
+
+        case "2":
+            // Method to view order
+            break;
+
+        case "3":
+            // Method to update order status
+            break;
+
+        case "4":
+            // Method to add comments
+            break;
+
+        case "5":
+            // Method to view comments
+            break;
+
+        case "0":
+            Console.WriteLine("Exiting...");
+            break;
+
+        default:
+            Console.WriteLine("Invalid option.");
+            break;
+    }
+
+} while (option != "0");
